92fc7a74d82d3a62d0a90a8b263155a816d42e52
RomanUstychenko <roman.ustychenko@gmail.com>
2022-09-11 18:47:33 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import { Profile } from "./Profile/Profile";
+import  user  from "./Json/user.json"
+import { Statistics } from "./Statistics/Statistics"
+import data from "./Json/data.json"
+
+// console.log (user)
 export const App = () => {
   return (
     <div
@@ -10,7 +16,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template HELLO
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+<Statistics stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+
+
+export const Statistics = ({ title, stats }) => {
+    const statsItems = stats.map(({ id, label, percentage }) => (
+        <ul class="stat-list">
+        <li class="item">
+          <span class="label">{label}</span>
+          <span class="percentage">{percentage}</span>
+        </li>
+        <li class="item">
+          <span class="label">{label}</span>
+          <span class="percentage">{percentage}</span>
+        </li>
+        <li class="item">
+          <span class="label">{label}</span>
+          <span class="percentage">{percentage}</span>
+        </li>
+        <li class="item">
+          <span class="label">{label}</span>
+          <span class="percentage">{percentage}</span>
+        </li>
+      </ul>
+    ));
+    return (
+        <section class="statistics">
+     <h2 class="title">Upload stats</h2>
+        {statsItems}
+   </section> 
+    )
+    }
+// export const Statistics = (data) => {
+//     const { title, stats } = data;
+//     return <section class="statistics">
+//     <h2 class="title">Upload stats</h2>
+
+//   </section> 
+
+// }
